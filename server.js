8fa824075123b8156676618936be96baf6d210ae
martinsaporiti <martinsaporiti@gmail.com>
2020-05-21 21:39:50 -0300

    Servicio para Recibir Mails

--- server.js
+++ server.js
@@ -70,6 +70,18 @@ server.post('/auth/login', (req, res) => {
     })
 })
 
+server.post('/mail', (req, res) => {
+  
+    const {Para, Asunto, Cuerpo} = req.body
+    
+    res.status(200).json({
+      'Para' : Para,
+      'Asunto' : Asunto,
+      'Cuerpo': Cuerpo
+    })
+    
+})
+
 
 server.get('/products/',  (req, res, next) => {
     console.log(req.headers.authorization)
